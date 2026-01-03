ae74536a0c201227370dd997257ff1b0cc56737b
avi0sprp-ux <avi0sprp@gmail.com>
2026-01-03 16:00:35 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+<BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/Game' element={<Game/>}/>
+        <Route path='/game' element={<Game/>}/>
+        <Route path='/wannaPlay' element={<WannaPlay/>}/>
+      </Routes>
+    </BrowserRouter>
